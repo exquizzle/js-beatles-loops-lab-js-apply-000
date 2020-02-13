892b123a0c5dcaeb606add45f10b72cdcf5b5980
Zi <germanychinaaustralia@yahoo.com>
2020-02-13 05:11:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments)  {
+  let emptyArr = [];
+  for ( let i = 0; i <musicians.length; i++ )  {
+    let newStr = `${musicians[i]} plays ${instruments[i]}`;
+    emptyArr.push(newString);
+  }
+  return emptyArr;
+}
