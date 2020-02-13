d8ea7560e8c1a14099f798cd70c83c5c824f6f1f
Zi <germanychinaaustralia@yahoo.com>
2020-02-13 05:11:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ function theBeatlesPlay(musicians, instruments)  {
   let emptyArr = [];
   for ( let i = 0; i <musicians.length; i++ )  {
     let newStr = `${musicians[i]} plays ${instruments[i]}`;
-    emptyArr.push(newString);
+    emptyArr.push(newStr);
   }
   return emptyArr;
 }
